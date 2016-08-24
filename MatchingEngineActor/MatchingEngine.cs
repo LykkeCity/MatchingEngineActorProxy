ee90b859265f665cb1f6a07b13401726c9b52701
Julia Tolipova <tuxina@yandex.ru>
2016-08-24 16:39:30 +0100

    generate random data for UpdateAssetPair event

--- MatchingEngineActor/MatchingEngine.cs
+++ MatchingEngineActor/MatchingEngine.cs
@@ -19,7 +19,10 @@ namespace MatchingEngineActor
         private StatefulServiceContext _context;
         private double _balance;
         private IActorTimer _updateTimer;
+        private IActorTimer _updateAssetTimer;
         private readonly IDictionaryService _dictionaryProxy;
+        private const double _bidPrice = 1.3253412;
+        private const double _askPrice = 1.3262112;
 
         public MatchingEngine(StatefulServiceContext context)
         {
@@ -43,6 +46,8 @@ namespace MatchingEngineActor
             _updateTimer = RegisterTimer(UpdateNumber, null, TimeSpan.FromSeconds(3),
                 TimeSpan.FromSeconds(3));
 
+            _updateAssetTimer = RegisterTimer(UpdateAssetPair, null, TimeSpan.FromSeconds(2),
+                TimeSpan.FromSeconds(2));
         }
 
         private Task UpdateNumber(object obj)
@@ -54,6 +59,34 @@ namespace MatchingEngineActor
             ev.BalanceUpdated(_balance);
 
             return TaskEx.Empty;
+        }
+
+        private async Task UpdateAssetPair(object obj)
+        {
+            var assetPairs = (await this.StateManager.GetStateAsync<IEnumerable<AssetPair>>("AssetPairs")).ToList();
+
+            var rnd = new Random();
+
+            var assetId = rnd.Next(assetPairs.Count);
+
+            var assetPair = assetPairs[assetId];
+
+            var significantDigit = Math.Pow(10, -assetPair.Accuracy);
+            var sign = rnd.Next(0, 1)*2 - 1;
+
+            var ask = Math.Round(_askPrice + (sign*significantDigit), assetPair.Accuracy);
+            var bid = Math.Round(_bidPrice + (sign*significantDigit), assetPair.Accuracy);
+
+            var feedData = new FeedData
+            {
+                Asset = assetPair.Name,
+                DateTime = DateTime.Now,
+                Ask = ask,
+                Bid = bid
+            };
+
+            var ev = GetEvent<IMatchingEngineEvents>();
+            ev.AssetPairUpdated(feedData);
         }
     }
 }
