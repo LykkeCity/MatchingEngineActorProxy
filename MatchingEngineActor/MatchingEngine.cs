544ea29816d6761e49d10b57872d394883eac349
Julia Tolipova <tuxina@yandex.ru>
2016-08-26 15:32:31 +0100

    updated core.domain package

--- MatchingEngineActor/MatchingEngine.cs
+++ MatchingEngineActor/MatchingEngine.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using System.Fabric;
 using System.Linq;
 using System.Threading.Tasks;
-using Common.Extenstions;
+using Lykke.Core.Domain.Account;
+using Lykke.Core.Domain.Account.Models;
+using Lykke.Core.Domain.Assets;
 using Lykke.Core.Domain.Assets.Models;
 using Lykke.Core.Domain.Dictionary;
-using Lykke.Core.Domain.Feed;
+using Lykke.Core.Domain.Exchange;
+using Lykke.Core.Domain.Exchange.Models;
 using Lykke.Core.Domain.MatchingEngine;
+using MatchingEngine.DataAccess.Account;
+using MatchingEngine.DataAccess.Asset;
+using MatchingEngine.DataAccess.Exchange;
+using MatchingEngine.Utils.Extensions;
 using MatchingEngineActor.Proxy;
 using Microsoft.ServiceFabric.Actors.Runtime;
 
@@ -16,18 +23,23 @@ namespace MatchingEngineActor
     [StatePersistence(StatePersistence.Persisted)]
     internal class MatchingEngine : Actor, IMatchingEngine
     {
-        private StatefulServiceContext _context;
-        private double _balance;
-        private IActorTimer _updateTimer;
-        private IActorTimer _updateAssetTimer;
+        private readonly IAccountInfoRepository _accountInfoRepository;
+        private readonly IAssetPairQuoteRepository _assetPairQuoteRepository;
+        private readonly IOrderInfoRepository _orderInfoRepository;
         private readonly IDictionaryService _dictionaryProxy;
-        private const double _bidPrice = 1.3253412;
-        private const double _askPrice = 1.3262112;
+        private StatefulServiceContext _context;
+        private IActorTimer _updateAssetTimer;
+        private IActorTimer _updateTimer;
 
         public MatchingEngine(StatefulServiceContext context)
         {
-            this._context = context;
+            _context = context;
             _dictionaryProxy = new DictionaryProxy().Connect(context);
+
+            //TODO: refactor
+            _accountInfoRepository = new AccountInfoRepository();
+            _assetPairQuoteRepository = new AssetPairQuoteRepository();
+            _orderInfoRepository = new OrderInfoRepository(_assetPairQuoteRepository);
         }
 
         public Task InitAsync()
@@ -35,13 +47,39 @@ namespace MatchingEngineActor
             return TaskEx.Empty;
         }
 
+        public Task<AccountInfo> GetAccountInfoAsync(string accountId)
+        {
+            return _accountInfoRepository.GetAsync(accountId);
+        }
+
+        public async Task OpenOrderAsync(string accountId, string assetPairId, double volume)
+        {
+            await _orderInfoRepository.AddAsync(accountId, assetPairId, volume);
+        }
+
+        public Task ClosePositionAsync(string accountId, string orderId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<OrderInfo>> GetActivePositionsAsync(string accountId)
+        {
+            return await _orderInfoRepository.GetAllAsync(accountId);
+        }
+
+        public async Task<IEnumerable<AssetPairQuote>> GetMarketProfile()
+        {
+            return await _assetPairQuoteRepository.GetAllAsync();
+        }
+
         protected override async Task OnActivateAsync()
         {
             ActorEventSource.Current.ActorMessage(this, "Actor activated.");
 
             var assetPairs = await _dictionaryProxy.GetAssetPairsAsync();
+            await _assetPairQuoteRepository.AddAllAsync(assetPairs);
 
-            await this.StateManager.TryAddStateAsync("AssetPairs", assetPairs);
+            await StateManager.TryAddStateAsync("AssetPairs", assetPairs);
 
             _updateTimer = RegisterTimer(UpdateNumber, null, TimeSpan.FromSeconds(3),
                 TimeSpan.FromSeconds(3));
@@ -50,20 +88,24 @@ namespace MatchingEngineActor
                 TimeSpan.FromSeconds(2));
         }
 
-        private Task UpdateNumber(object obj)
+        private async Task UpdateNumber(object obj)
         {
             var rnd = new Random();
 
-            _balance = rnd.NextDouble()*100;
-            var ev = GetEvent<IMatchingEngineEvents>();
-            ev.BalanceUpdated(_balance);
+            var accountInfos = await _accountInfoRepository.GetAllAsync();
 
-            return TaskEx.Empty;
+            foreach (var account in accountInfos)
+            {
+                account.Balance = rnd.NextDouble()*100;
+
+                var ev = GetEvent<IMatchingEngineEvents>();
+                ev.AccountUpdated(account.AccountId);
+            }
         }
 
         private async Task UpdateAssetPair(object obj)
         {
-            var assetPairs = (await this.StateManager.GetStateAsync<IEnumerable<AssetPair>>("AssetPairs")).ToList();
+            var assetPairs = (await StateManager.GetStateAsync<IEnumerable<AssetPair>>("AssetPairs")).ToList();
 
             var rnd = new Random();
 
@@ -71,22 +113,10 @@ namespace MatchingEngineActor
 
             var assetPair = assetPairs[assetId];
 
-            var significantDigit = Math.Pow(10, -assetPair.Accuracy);
-            var sign = rnd.Next(0, 1)*2 - 1;
-
-            var ask = Math.Round(_askPrice + (sign*significantDigit), assetPair.Accuracy);
-            var bid = Math.Round(_bidPrice + (sign*significantDigit), assetPair.Accuracy);
-
-            var feedData = new FeedData
-            {
-                Asset = assetPair.Name,
-                DateTime = DateTime.Now,
-                Ask = ask,
-                Bid = bid
-            };
+            var updatedQuote = await _assetPairQuoteRepository.UpdateAsync(assetPair);
 
             var ev = GetEvent<IMatchingEngineEvents>();
-            ev.AssetPairPriceUpdated(feedData);
+            ev.AssetPairPriceUpdated(updatedQuote);
         }
     }
 }
