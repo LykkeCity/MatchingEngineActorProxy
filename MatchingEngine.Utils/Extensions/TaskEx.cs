30b1a441b5fdf63fcf614a406a3e11f9146d4df1
Julia Tolipova <tuxina@yandex.ru>
2016-08-26 15:31:53 +0100

    cleanup

--- MatchingEngine.Utils/Extensions/TaskEx.cs
+++ MatchingEngine.Utils/Extensions/TaskEx.cs
@@ -2,7 +2,7 @@
 using System.Runtime.ExceptionServices;
 using System.Threading.Tasks;
 
-namespace Common.Extenstions
+namespace MatchingEngine.Utils.Extensions
 {
     public static class TaskEx
     {
