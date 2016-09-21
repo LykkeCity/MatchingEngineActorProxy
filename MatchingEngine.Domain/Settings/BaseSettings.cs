2995b375293c0db64f7acafa43c37d02ace87932
Julia Tolipova <tuxina@yandex.ru>
2016-09-21 14:51:21 +0100

    service-bus: added azure storage to read settings from bd

---
+++ MatchingEngine.Domain/Settings/BaseSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace MatchingEngine.Domain.Settings
+{
+    public class BaseSettings
+    {
+        public MatchingOrdersSettings MatchingEngine { get; set; }
+    }
+
+    public class MatchingOrdersSettings
+    {
+        public string ServiceBusConnectionString { get; set; }
+    }
+}
